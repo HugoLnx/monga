a69a4f0a87dfe7825e4358daf1833805acbf7226
Hugo Roque <hugolnx@gmail.com>
2015-05-11 06:28:57 -0300

    run tests including ast-tree tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,6 +3,9 @@ echo "Running lexical Tests"
 python scripts/lexical.py test_all
 
 echo
-
 echo "Running syntax Tests"
 python scripts/syntax.py test_all
+
+echo
+echo "Running ast-tree Tests"
+python scripts/ast-tree.py test_all
