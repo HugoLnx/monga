28dfcace0293954b3b0b90d428a54cf5f79a0ad7
Hugo Roque <hugolnx@gmail.com>
2015-06-26 01:04:34 -0300

    removing function signature

--- src/assembly_writer.h
+++ src/assembly_writer.h
@@ -3,7 +3,6 @@ void ASY_globalVar(char *varName, char *varTag, char *varValue);
 void ASY_function(char *funcName);
 void ASY_label(char *labelName);
 void ASY_functionBeginning();
-void ASY_functionHeader();
 void ASY_functionCall(char *funcName, int paramsStackSize);
 void ASY_functionEnding();
 void ASY_raw(char *fmt, ...);
